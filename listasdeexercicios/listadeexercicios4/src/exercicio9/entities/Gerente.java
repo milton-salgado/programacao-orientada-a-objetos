7b5acc4634a2b5f2436296f5f58915890b59bd3b
milton-salgado <miltonsalgadoleandro@gmail.com>
2024-05-08 21:01:30 -0300

    Correção de código na Lista de Exercícios 4

--- listasdeexercicios/listadeexercicios4/src/exercicio9/entities/Gerente.java
+++ listasdeexercicios/listadeexercicios4/src/exercicio9/entities/Gerente.java
@@ -11,6 +11,6 @@ public class Gerente extends Funcionario {
     public double bonus(Trimestre t) {
         // Chamando bonus() do pai, pois se não, ele chama o próprio bonus, criando um efeito recursivo, visto que no exercício 7 o trimestre é o 3o, e não o 4o
         // Agora o código não compila pois salario é private em Funcionario, e não protected, no exercício 8.
-        return (t == Trimestre._4o) ? 2 * 0.15 : super.bonus(t);
+        return (t == Trimestre._4o) ? salario * 0.15 : super.bonus(t);
     }
 }
